5fbf7f1a6b9875edd23d9c8592c5fcf4cb870265
James Tasker <james_tasker1@hotmail.com>
2015-02-18 14:35:52 +0000

    Move LCD pins to purely digital (non-PWM) pins
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- CocktailMaker/CocktailMaker.ino
+++ CocktailMaker/CocktailMaker.ino
@@ -1,11 +1,11 @@
 /*
   The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ * LCD RS pin to digital pin 13
+ * LCD Enable pin to digital pin 12
+ * LCD D4 pin to digital pin 4
+ * LCD D5 pin to digital pin 2
+ * LCD D6 pin to digital pin 1
+ * LCD D7 pin to digital pin 0
  * LCD R/W pin to ground
  * 5K resistor:
  * ends to +5V and ground
@@ -17,7 +17,7 @@
 
 #define SCROLL_DELAY 1000
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(13, 12, 4, 2, 1, 0);
 
 bool mLastButtonState = false;
 String mAvailableIngredients[] = {"Pineapple Juice", "Vodka", "Cointreau", "Cranberry Juice", "Lime Juice", "Peach Schnapps"};
